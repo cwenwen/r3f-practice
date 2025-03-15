921ad5de6efafea27d673700de7b4f1001c5353c
cwenwen <mswenwencheng@gmail.com>
2025-03-15 17:09:02 +0800

    fix: change chunking logic

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,11 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react-vendor';
-            if (id.includes('lodash')) return 'lodash-vendor';
-            return 'vendor';
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
           }
         },
       },
