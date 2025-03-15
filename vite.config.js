42a3f27fc901b607f687067cb3c0b4ded3279a6c
cwenwen <mswenwencheng@gmail.com>
2025-03-15 17:05:32 +0800

    feat: configure for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,20 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/r3f-practice/',
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor';
+            if (id.includes('lodash')) return 'lodash-vendor';
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
