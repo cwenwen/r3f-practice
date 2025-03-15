7e6681368c6d8cea3c77aa1edb224066ea15f627
cwenwen <mswenwencheng@gmail.com>
2025-03-15 21:36:13 +0800

    fix(Instructions): enhance responsiveness

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -20,7 +20,7 @@ export default function Instructions() {
 
   return (
     <div
-      className={`absolute bottom-1/4 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-800 px-4 py-2 text-2xl text-white transition-opacity duration-1000 ${visible ? 'opacity-50' : 'opacity-0'}`}
+      className={`absolute bottom-1/4 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-800 px-4 py-2 text-xl text-nowrap text-white transition-opacity duration-1000 md:bottom-1/3 md:text-2xl lg:bottom-1/5 ${visible ? 'opacity-50' : 'opacity-0'}`}
     >
       <p>🎥 Scroll to zoom in/out</p>
       <p>🕹 Drag to look around</p>
